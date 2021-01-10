9152d92ec4564f79ed7afd25c59fa30bc84caa55
Aypar Yılmazkaya <ayparyilmazkaya@gmail.com>
2021-01-10 18:14:55 +0300

    CHANGE records.model properties

--- models/records.model.js
+++ models/records.model.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-const RecordsSchema = new Schema({})
+const RecordsSchema = new Schema({
+    _id: Schema.ObjectId,
+    key: String,
+    value: String,
+    createdAt: Date,
+    counts: [Number]
+})
 const Records = mongoose.model('Records', RecordsSchema)
 module.exports = Records;
